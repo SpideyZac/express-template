f5298ab4f1eb44e7b730938dc10e652bce202f76
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-01-12 17:39:57 -0800

    v1.0.0

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,7 +1,7 @@
 export function createLogFunction(fileName: string): (...args: any[]) => void {
-    return function log(...args: any[]) {
-        if (args.length > 1) {
-            console.log(`[${fileName}-${args[0]}]`, ...args.slice(1));
+    return function log(functionNameInfo: string, ...args: any[]) {
+        if (args.length > 0) {
+            console.log(`[${fileName}-${functionNameInfo}]`, ...args);
         } else {
             throw Error(`[${fileName}-log] The number of args has to be greater than 1!`);
         }
